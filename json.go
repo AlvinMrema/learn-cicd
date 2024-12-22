f6ba2920f10c532b4e7ebba000b9361d8a029c42
AlvinMrema <sonalpha023@gmail.com>
2024-12-22 14:44:07 +0300

    fixed gosec issues for CI

--- json.go
+++ json.go
@@ -27,5 +27,9 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	w.Write(dat)
+	_, err = w.Write(dat)
+	if err != nil {
+		log.Printf("Error failed to write data: %s", err)
+		w.WriteHeader(500)
+	}
 }
